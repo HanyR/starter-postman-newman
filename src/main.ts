5f96d35ed0e9e3f8da177ead8ab0c91aca8172fc
hanniarojas <hany.cristi@gmail.com>
2023-01-13 00:22:04 -0600

    test:postman newman integration

---
+++ src/main.ts
@@ -0,0 +1,12 @@
+import newman from 'newman';
+//const newman = require('newman'); // require newman in your project
+
+// call newman.run to pass `options` object and wait for callback
+newman.run({
+    collection: require('./collections/trello_dev_env.postman_collection.json'), 
+    environment: require('./collections/trello_dev.postman_environment.json'), 
+    reporters: 'junit',
+}, function (err) {
+	if (err) { throw err; }
+    console.log('collection run complete!');
+});
