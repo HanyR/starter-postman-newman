b0d62a21577913d4b585eaf8364c26bc079b2394
hanniarojas <hany.cristi@gmail.com>
2023-01-13 00:57:33 -0600

    Signed-off-by: hanniarojas <hany.cristi@gmail.com>

--- src/main.ts
+++ src/main.ts
@@ -2,6 +2,7 @@ import newman from 'newman';
 //const newman = require('newman'); // require newman in your project
 
 // call newman.run to pass `options` object and wait for callback
+
 newman.run({
     collection: require('./collections/trello_dev_env.postman_collection.json'), 
     environment: require('./collections/trello_dev.postman_environment.json'), 
